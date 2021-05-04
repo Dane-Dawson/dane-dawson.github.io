7bf3162c084e05ee1eaa68fbf3cf91d1c5391383
Your Name <you@example.com>
2021-05-04 16:54:42 -0500

    abstracted out terminal readouts into separate file

--- src/components/terminal/Terminal.js
+++ src/components/terminal/Terminal.js
@@ -1,6 +1,8 @@
 import './Terminal.css';
 import React, { useState } from 'react';
 
+import {commands, help, limmerick} from "./TerminalDisplayReadouts"
+
 function Terminal(props) {
     const [terminalDisplay, setTerminalDisplay] = useState( '')
 
@@ -16,30 +18,14 @@ function Terminal(props) {
 
     const handleTerminalDisplay = () => {
         switch (terminalDisplay){
-            case "help":
-                return (<div>
-                <p className="terminal-display">I mean...I wish I could, I need some myself!</p>
-                <p className="terminal-display help-1">Have you tried using Google?</p>
-                </div>)
             case "clear":
                 return "";
+            case "help":
+                return help()
             case "commands":
-                return (<div>
-                <p className="terminal-display commands-1">Seriously? Just look up like...2 lines.</p>
-                <p className="terminal-display commands-2">I know devs don't read documentation, but come on now.</p>
-
-                </div>)
+                return commands()
             case "limmerick":
-                return(<div>
-                    <p className="terminal-display limmerick-1">There once was a coder who came</p>
-                    <p className="terminal-display limmerick-2">to a site engineered by a Dane.</p>
-                    <p className="terminal-display limmerick-3">His work is unique</p>
-                    <p className="terminal-display limmerick-4">and he's up for critique,</p>
-                    <p className="terminal-display limmerick-5">but be gentle to cause him no pain.</p>
-                    <br/>
-                    <br/>
-                    <p className="terminal-display limmerick-6">...please?</p>
-                </div>)
+                return limmerick()
         }
     }
 
---
+++ src/components/terminal/TerminalDisplayReadouts.js
@@ -0,0 +1,28 @@
+export const commands = () => {
+    return (<div>
+        <p className="terminal-display commands-1">Seriously? Just look up like...2 lines.</p>
+        <p className="terminal-display commands-2">I know devs don't read documentation, but come on now.</p>
+
+        </div>)
+}
+
+export const help = () => {
+    return (<div>
+        <p className="terminal-display">I mean...I wish I could, I need some myself!</p>
+        <p className="terminal-display help-1">Have you tried using Google?</p>
+        </div>)
+}
+
+export const limmerick = () => {
+    return (<div>
+                    <p className="terminal-display limmerick-1">There once was a coder who came</p>
+                    <p className="terminal-display limmerick-2">to a site engineered by a Dane.</p>
+                    <p className="terminal-display limmerick-3">His work is unique</p>
+                    <p className="terminal-display limmerick-4">and he's up for critique,</p>
+                    <p className="terminal-display limmerick-5">but be gentle to cause him no pain.</p>
+                    <br/>
+                    <br/>
+                    <p className="terminal-display limmerick-6">...please?</p>
+        </div>
+    )
+}
